0aa4d5f09ac83f40b4dae42d07c959decea4f7c8
achmadk7777 <45935574+achmadk7777@users.noreply.github.com>
2018-12-17 16:34:39 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+
+const TOKEN = 'NTI0MTI2MDI2MDM3NTkyMDY0.DvjrQw.W-_rDUdIjsTyNS37WM0chW0zVV8';
+
+const fs = require("fs");
+
+// global setting
+const prefix = '</';
+
+var bot = new Discord.Client();
+
+bot.on('message', async message => {
+
+    let msg = message.content.toUpperCase();
+    let sender = message.author;
+    let args = message.content.slice(prefix.length).trim().split(/ + /g);
+    let command = args.shift().toLowerCase();
+
+
+    if (msg === prefix + 'PING') {
+        message.channel.send('PONG')
+    }
+
+    var dogs  = ["321321321", "565665655", "21323232323", "4564564311231", "2121548787", "98798989898"]
+    var throwdog = dogs[Math.floor(Math.random() * dogs.length)]
+
+    if (msg === prefix + 'DOG') {
+        message.channel.send(`nah ini dia randomnya! \n${throwdog}`);
+    }
+
+})
+
+bot.on("ready", function() {
+    console.log('Ready');
+});
+
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+    
+    if (message.content == "Hello") {
+        message.channel.sendMessage("Hai juga");
+    }
+});
+
+bot.login(process.env.BOT_TOKEN); 
